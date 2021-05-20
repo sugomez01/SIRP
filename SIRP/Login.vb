d26a788bf88b2baec1535c0e2b63ac7f00afc521
sugomez01 <sgomezriquelme@gmail.com>
2021-05-20 01:01:49 -0400

    login validadoqueda bonito

--- SIRP/Login.vb
+++ SIRP/Login.vb
@@ -1,28 +1,47 @@
-﻿Public Class Login
+﻿
+Public Class Login
     Dim id_int, op As Integer
+    Dim conexion As conexion = New conexion()
+
 
     Private Sub Login_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        'conexion.Conectar()
+        conexion.Conectar()
         ControlBox = False
         txtPass.PasswordChar = "*"
     End Sub
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnIngresar.Click
-        id_int = txtPass.Text
 
-        If id_int = 1 Then
-            Principal.picPrincipal.Image = Image.FromFile("C:\Users\sgome\Desktop\PROYECTO SIRP\SIRP\IMAGES\carabineros.png")
-        ElseIf id_int = 2 Then
-            Principal.picPrincipal.Image = Image.FromFile("C:\Users\sgome\Desktop\PROYECTO SIRP\SIRP\IMAGES\os-10.png")
-        Else
-            Principal.picPrincipal.Image = Image.FromFile("C:\Users\sgome\Desktop\PROYECTO SIRP\SIRP\IMAGES\pdi.png")
-        End If
+        Try
+            If conexion.usuarioRegistrado(txtUser.Text) = True Then
+                Dim contra As String = conexion.contrasena(txtUser.Text)
+                Dim tipoInstitucion As String = conexion.tipoInstitucion(txtUser.Text)
 
-        Principal.Show()
-        Me.Hide()
+                If contra = txtPass.Text Then
+                    MsgBox("Bienvenido a conexion")
+                    If conexion.tipoUsuario(txtUser.Text) = 1 Then
+                        MsgBox("administrador y su institucion es" + tipoInstitucion)
+                    ElseIf conexion.tipoUsuario(txtUser.Text) = 2 Then
+                        MsgBox("Jefe de Zona y su institucion es" + tipoInstitucion)
+                    Else
+                        MsgBox("Operador´y su institucion es" + tipoInstitucion)
+                    End If
+                Else
+                    MsgBox("Usuario o contraseña Incorrectas" + txtPass.Text + " ** " + contra)
+
+                End If
+            End If
+        Catch ex As Exception
+            MsgBox(ex.ToString)
+        End Try
+
     End Sub
 
-    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
+<<<<<<< HEAD
+    Private Sub Label3_Click(sender As Object, e As EventArgs)
+=======
+    Private Sub Label3_Click(sender As Object, e As EventArgs) 
+>>>>>>> 70254a2f638ff4c8a3887eec2f5e21262f37ffc0
 
     End Sub
 
--- SIRP/conexion.vb
+++ SIRP/conexion.vb
@@ -3,28 +3,102 @@ Imports System.Data.SqlClient
 Imports System.Windows.Forms
 
 Public Class conexion
-
-    Public conexion As SqlConnection = New SqlConnection("Data Source=DESKTOP-EUII0N8;User ID=sa;Password=sasa;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False")
+    ' descomentar segun pc
+    Public conexion As SqlConnection = New SqlConnection("Data Source=LAPTOP-6GF7OE4K;Initial Catalog=SIRP;Integrated Security=True")
 
     Private cmb As SqlCommandBuilder
-    Public ds As DataSet = New DataSet()
+    Public ds As DataSet = New DataSet
     Public da As SqlDataAdapter
     Public comando As SqlCommand
+    Public dr As SqlDataReader
+    Public dt As DataTable
 
     'Conexión a la DB
     Public Sub Conectar()
         Try
             conexion.Open()
-            MessageBox.Show("Conectado")
+            '    MessageBox.Show("Conectado")
         Catch ex As Exception
-            MessageBox.Show("Error al conectar")
+            '  MessageBox.Show("Error al conectar")
         Finally
             conexion.Close()
         End Try
     End Sub
 
 
-    Public Sub Consulta(ByVal sql As String, ByVal tabla As String)
+    Function usuarioRegistrado(ByVal nombreUSuario As String)
+        conexion.Close()
+        conexion.Open()
+        Dim resultado As Boolean = False
+        Try
+            comando = New SqlCommand("select * from l_usuario where username = '" + nombreUSuario + "'", conexion)
+            dr = comando.ExecuteReader
+            If dr.Read Then
+                resultado = True
+            End If
+            dr.Close()
+        Catch ex As Exception
+            MsgBox(ex.ToString)
+        End Try
+        Return resultado
 
-    End Sub
+    End Function
+
+    Function contrasena(ByVal nombreUSuario As String)
+        conexion.Close()
+        conexion.Open()
+        Dim resultado As String = ""
+        Try
+            comando = New SqlCommand("select password from l_usuario where username = '" + nombreUSuario + "'", conexion)
+            dr = comando.ExecuteReader
+
+            If dr.Read Then
+                resultado = dr.Item("password")
+            End If
+            dr.Close()
+        Catch ex As Exception
+            MsgBox(ex.ToString)
+        End Try
+        Return resultado
+
+    End Function
+
+    Function tipoUsuario(ByVal nombreUSuario As String) As Integer
+        conexion.Close()
+        conexion.Open()
+        Dim resultado As String = ""
+        Try
+            comando = New SqlCommand("select l_id_tipo_user from l_usuario where username = '" + nombreUSuario + "'", conexion)
+            dr = comando.ExecuteReader
+
+            If dr.Read Then
+                resultado = CInt(dr.Item("l_id_tipo_user"))
+            End If
+            dr.Close()
+        Catch ex As Exception
+            MsgBox(ex.ToString)
+        End Try
+        Return resultado
+
+    End Function
+
+    Function tipoInstitucion(ByVal nombreUSuario As String) As Integer
+        conexion.Close()
+        conexion.Open()
+        Dim resultado As String = ""
+        Try
+            comando = New SqlCommand("select l_id_institucion from l_usuario where username = '" + nombreUSuario + "'", conexion)
+            dr = comando.ExecuteReader
+
+            If dr.Read Then
+                resultado = CInt(dr.Item("l_id_institucion"))
+            End If
+            dr.Close()
+        Catch ex As Exception
+            MsgBox(ex.ToString)
+        End Try
+        Return resultado
+
+    End Function
+
 End Class
