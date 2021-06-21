4e0fb58caa9650143781807347c8eda4e4da2f79
matiasfvs <valenzuela.solorza.n@gmail.com>
2021-06-21 05:01:53 -0400

    REPORTES LISTOS

--- SIRP/ModuloReportes.Designer.vb
+++ SIRP/ModuloReportes.Designer.vb
@@ -35,6 +35,17 @@ Partial Class ModuloReportes
         Me.dgvReporte = New System.Windows.Forms.DataGridView()
         Me.Button1 = New System.Windows.Forms.Button()
         Me.Button2 = New System.Windows.Forms.Button()
+        Me.rbtnReporte1 = New System.Windows.Forms.RadioButton()
+        Me.rbtnReporte2 = New System.Windows.Forms.RadioButton()
+        Me.rbtnReporte3 = New System.Windows.Forms.RadioButton()
+        Me.rbtnReporte4 = New System.Windows.Forms.RadioButton()
+        Me.rbtnReporte5 = New System.Windows.Forms.RadioButton()
+        Me.DateTimePicker1 = New System.Windows.Forms.DateTimePicker()
+        Me.DateTimePicker2 = New System.Windows.Forms.DateTimePicker()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.rbtnReporte6 = New System.Windows.Forms.RadioButton()
+        Me.lblFechaIni = New System.Windows.Forms.Label()
+        Me.lblFechaFin = New System.Windows.Forms.Label()
         Me.MenuStrip1.SuspendLayout()
         CType(Me.LdelincuenteBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.SIRPDataSet, System.ComponentModel.ISupportInitialize).BeginInit()
@@ -54,14 +65,14 @@ Partial Class ModuloReportes
         'OpcionesToolStripMenuItem
         '
         Me.OpcionesToolStripMenuItem.Name = "OpcionesToolStripMenuItem"
-        Me.OpcionesToolStripMenuItem.Size = New System.Drawing.Size(85, 24)
+        Me.OpcionesToolStripMenuItem.Size = New System.Drawing.Size(85, 26)
         Me.OpcionesToolStripMenuItem.Text = "Opciones"
         '
         'VolverToolStripMenuItem
         '
         Me.VolverToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.VolverAlMeúPrincipalToolStripMenuItem, Me.SalirDelSistemaToolStripMenuItem})
         Me.VolverToolStripMenuItem.Name = "VolverToolStripMenuItem"
-        Me.VolverToolStripMenuItem.Size = New System.Drawing.Size(64, 24)
+        Me.VolverToolStripMenuItem.Size = New System.Drawing.Size(64, 26)
         Me.VolverToolStripMenuItem.Text = "Volver"
         '
         'VolverAlMeúPrincipalToolStripMenuItem
@@ -94,7 +105,7 @@ Partial Class ModuloReportes
         '
         Me.lblTitulo.AutoSize = True
         Me.lblTitulo.Font = New System.Drawing.Font("Calibri", 13.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.lblTitulo.Location = New System.Drawing.Point(530, 55)
+        Me.lblTitulo.Location = New System.Drawing.Point(514, 28)
         Me.lblTitulo.Name = "lblTitulo"
         Me.lblTitulo.Size = New System.Drawing.Size(94, 29)
         Me.lblTitulo.TabIndex = 2
@@ -103,36 +114,159 @@ Partial Class ModuloReportes
         'dgvReporte
         '
         Me.dgvReporte.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.dgvReporte.Location = New System.Drawing.Point(241, 113)
+        Me.dgvReporte.Location = New System.Drawing.Point(264, 87)
         Me.dgvReporte.Name = "dgvReporte"
         Me.dgvReporte.RowHeadersWidth = 51
         Me.dgvReporte.RowTemplate.Height = 24
-        Me.dgvReporte.Size = New System.Drawing.Size(916, 311)
+        Me.dgvReporte.Size = New System.Drawing.Size(908, 311)
         Me.dgvReporte.TabIndex = 3
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(32, 126)
+        Me.Button1.Font = New System.Drawing.Font("Segoe UI", 10.2!)
+        Me.Button1.Location = New System.Drawing.Point(32, 406)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.Size = New System.Drawing.Size(88, 34)
         Me.Button1.TabIndex = 4
-        Me.Button1.Text = "Button1"
+        Me.Button1.Text = "Generar "
         Me.Button1.UseVisualStyleBackColor = True
         '
         'Button2
         '
-        Me.Button2.Location = New System.Drawing.Point(32, 191)
+        Me.Button2.Font = New System.Drawing.Font("Segoe UI", 10.2!)
+        Me.Button2.Location = New System.Drawing.Point(142, 406)
         Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.Size = New System.Drawing.Size(98, 34)
         Me.Button2.TabIndex = 5
-        Me.Button2.Text = "Button2"
+        Me.Button2.Text = "Descargar"
         Me.Button2.UseVisualStyleBackColor = True
+        '
+        'rbtnReporte1
+        '
+        Me.rbtnReporte1.AutoSize = True
+        Me.rbtnReporte1.Checked = True
+        Me.rbtnReporte1.Location = New System.Drawing.Point(32, 87)
+        Me.rbtnReporte1.Name = "rbtnReporte1"
+        Me.rbtnReporte1.Size = New System.Drawing.Size(198, 21)
+        Me.rbtnReporte1.TabIndex = 6
+        Me.rbtnReporte1.TabStop = True
+        Me.rbtnReporte1.Text = "Ordenado Alfabeticamente"
+        Me.rbtnReporte1.UseVisualStyleBackColor = True
+        '
+        'rbtnReporte2
+        '
+        Me.rbtnReporte2.AutoSize = True
+        Me.rbtnReporte2.Location = New System.Drawing.Point(32, 114)
+        Me.rbtnReporte2.Name = "rbtnReporte2"
+        Me.rbtnReporte2.Size = New System.Drawing.Size(150, 21)
+        Me.rbtnReporte2.TabIndex = 7
+        Me.rbtnReporte2.TabStop = True
+        Me.rbtnReporte2.Text = "Por delito cometido"
+        Me.rbtnReporte2.UseVisualStyleBackColor = True
+        '
+        'rbtnReporte3
+        '
+        Me.rbtnReporte3.AutoSize = True
+        Me.rbtnReporte3.Location = New System.Drawing.Point(32, 141)
+        Me.rbtnReporte3.Name = "rbtnReporte3"
+        Me.rbtnReporte3.Size = New System.Drawing.Size(201, 21)
+        Me.rbtnReporte3.TabIndex = 8
+        Me.rbtnReporte3.TabStop = True
+        Me.rbtnReporte3.Text = "Por Comuna de Residencia"
+        Me.rbtnReporte3.UseVisualStyleBackColor = True
+        '
+        'rbtnReporte4
+        '
+        Me.rbtnReporte4.AutoSize = True
+        Me.rbtnReporte4.Location = New System.Drawing.Point(32, 168)
+        Me.rbtnReporte4.Name = "rbtnReporte4"
+        Me.rbtnReporte4.Size = New System.Drawing.Size(127, 21)
+        Me.rbtnReporte4.TabIndex = 9
+        Me.rbtnReporte4.TabStop = True
+        Me.rbtnReporte4.Text = "Ultima vez visto"
+        Me.rbtnReporte4.UseVisualStyleBackColor = True
+        '
+        'rbtnReporte5
+        '
+        Me.rbtnReporte5.AutoSize = True
+        Me.rbtnReporte5.Location = New System.Drawing.Point(32, 195)
+        Me.rbtnReporte5.Name = "rbtnReporte5"
+        Me.rbtnReporte5.Size = New System.Drawing.Size(126, 21)
+        Me.rbtnReporte5.TabIndex = 10
+        Me.rbtnReporte5.TabStop = True
+        Me.rbtnReporte5.Text = "Por parentesco"
+        Me.rbtnReporte5.UseVisualStyleBackColor = True
+        '
+        'DateTimePicker1
+        '
+        Me.DateTimePicker1.CalendarFont = New System.Drawing.Font("Segoe UI", 11.25!)
+        Me.DateTimePicker1.Location = New System.Drawing.Point(30, 289)
+        Me.DateTimePicker1.Name = "DateTimePicker1"
+        Me.DateTimePicker1.Size = New System.Drawing.Size(200, 22)
+        Me.DateTimePicker1.TabIndex = 11
+        '
+        'DateTimePicker2
+        '
+        Me.DateTimePicker2.CalendarFont = New System.Drawing.Font("Segoe UI", 11.25!)
+        Me.DateTimePicker2.Location = New System.Drawing.Point(30, 348)
+        Me.DateTimePicker2.Name = "DateTimePicker2"
+        Me.DateTimePicker2.Size = New System.Drawing.Size(200, 22)
+        Me.DateTimePicker2.TabIndex = 12
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(29, 64)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(91, 17)
+        Me.Label1.TabIndex = 14
+        Me.Label1.Text = "Delincuente :"
+        '
+        'rbtnReporte6
+        '
+        Me.rbtnReporte6.AutoSize = True
+        Me.rbtnReporte6.Location = New System.Drawing.Point(32, 222)
+        Me.rbtnReporte6.Name = "rbtnReporte6"
+        Me.rbtnReporte6.Size = New System.Drawing.Size(156, 21)
+        Me.rbtnReporte6.TabIndex = 15
+        Me.rbtnReporte6.TabStop = True
+        Me.rbtnReporte6.Text = "Delito por Ubicacion"
+        Me.rbtnReporte6.UseVisualStyleBackColor = True
+        '
+        'lblFechaIni
+        '
+        Me.lblFechaIni.AutoSize = True
+        Me.lblFechaIni.Location = New System.Drawing.Point(29, 268)
+        Me.lblFechaIni.Name = "lblFechaIni"
+        Me.lblFechaIni.Size = New System.Drawing.Size(83, 17)
+        Me.lblFechaIni.TabIndex = 16
+        Me.lblFechaIni.Text = "Fecha Inicio"
+        '
+        'lblFechaFin
+        '
+        Me.lblFechaFin.AutoSize = True
+        Me.lblFechaFin.Location = New System.Drawing.Point(29, 326)
+        Me.lblFechaFin.Name = "lblFechaFin"
+        Me.lblFechaFin.Size = New System.Drawing.Size(70, 17)
+        Me.lblFechaFin.TabIndex = 17
+        Me.lblFechaFin.Text = "Fecha Fin"
         '
         'ModuloReportes
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(1184, 476)
+        Me.Controls.Add(Me.lblFechaFin)
+        Me.Controls.Add(Me.lblFechaIni)
+        Me.Controls.Add(Me.rbtnReporte6)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.DateTimePicker2)
+        Me.Controls.Add(Me.DateTimePicker1)
+        Me.Controls.Add(Me.rbtnReporte5)
+        Me.Controls.Add(Me.rbtnReporte4)
+        Me.Controls.Add(Me.rbtnReporte3)
+        Me.Controls.Add(Me.rbtnReporte2)
+        Me.Controls.Add(Me.rbtnReporte1)
         Me.Controls.Add(Me.Button2)
         Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.dgvReporte)
@@ -163,4 +297,15 @@ Partial Class ModuloReportes
     Friend WithEvents dgvReporte As DataGridView
     Friend WithEvents Button1 As Button
     Friend WithEvents Button2 As Button
+    Friend WithEvents rbtnReporte1 As RadioButton
+    Friend WithEvents rbtnReporte2 As RadioButton
+    Friend WithEvents rbtnReporte3 As RadioButton
+    Friend WithEvents rbtnReporte4 As RadioButton
+    Friend WithEvents rbtnReporte5 As RadioButton
+    Friend WithEvents DateTimePicker1 As DateTimePicker
+    Friend WithEvents DateTimePicker2 As DateTimePicker
+    Friend WithEvents Label1 As Label
+    Friend WithEvents rbtnReporte6 As RadioButton
+    Friend WithEvents lblFechaIni As Label
+    Friend WithEvents lblFechaFin As Label
 End Class
--- SIRP/ModuloReportes.vb
+++ SIRP/ModuloReportes.vb
@@ -4,6 +4,8 @@
 
 Public Class ModuloReportes
 
+    Dim fecha_ini, fecha_fin, reporte1 As String
+
     Public conn As SqlConnection = New SqlConnection("Data Source=LAPTOP-6GF7OE4K;Initial Catalog=SIRP;Integrated Security=True")
     'Public conn As SqlConnection = New SqlConnection("Data Source=DESKTOP-EUII0N8;User ID=sa;Password=sasa;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False")
 
@@ -18,24 +20,177 @@ Public Class ModuloReportes
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
 
-        comando = New SqlCommand("select * from b_detalle_delito", conn)
-        da = New SqlDataAdapter(comando)
-        dt = New DataTable()
-        da.Fill(ds)
+        If rbtnReporte6.Checked = True Then
+            fecha_ini = Format(DateTimePicker1.Value, "yyyy/MM/dd").ToString()
+            fecha_fin = Format(DateTimePicker2.Value, "yyyy/MM/dd").ToString()
+        End If
 
-        dgvReporte.DataSource = ds.Tables(0).DefaultView
+
+        ds.Tables.Clear()
+
+        If rbtnReporte1.Checked = True Then
+            comando = New SqlCommand("select * from l_delincuente order by desc_apellido,desc_nombre", conn)
+            da = New SqlDataAdapter(comando)
+            dt = New DataTable()
+            da.Fill(ds)
+
+            dgvReporte.DataSource = ds.Tables(0).DefaultView
+
+        ElseIf rbtnReporte2.Checked = True Then
+
+            comando = New SqlCommand("select * from tb_rep_delito_cometido", conn)
+            da = New SqlDataAdapter(comando)
+            dt = New DataTable()
+            da.Fill(ds)
+
+            dgvReporte.DataSource = ds.Tables(0).DefaultView
+
+        ElseIf rbtnReporte3.Checked = True Then
+
+            comando = New SqlCommand("select * from tb_rep_delincuente_comuna", conn)
+            da = New SqlDataAdapter(comando)
+            dt = New DataTable()
+            da.Fill(ds)
+
+            dgvReporte.DataSource = ds.Tables(0).DefaultView
+
+        ElseIf rbtnReporte4.Checked = True Then
+
+            comando = New SqlCommand("select * from tb_rep_delincuente_ultima_vez", conn)
+            da = New SqlDataAdapter(comando)
+            dt = New DataTable()
+            da.Fill(ds)
+
+            dgvReporte.DataSource = ds.Tables(0).DefaultView
+
+        ElseIf rbtnReporte5.Checked = True Then
+
+            comando = New SqlCommand("select * from tb_rep_delincuente_parentesco order by desc_banda", conn)
+            da = New SqlDataAdapter(comando)
+            dt = New DataTable()
+            da.Fill(ds)
+
+            dgvReporte.DataSource = ds.Tables(0).DefaultView
+
+        ElseIf rbtnReporte6.Checked = True Then
+
+            Dim query
+            query = "select * from tb_rep_delito_ubicacion where fecha_delito between '" + fecha_ini + "' and '" + fecha_fin + "' order by desc_delito"
+            comando = New SqlCommand("select * from tb_rep_delito_ubicacion where fecha_delito between '" + fecha_ini + "' and '" + fecha_fin + "' order by desc_delito", conn)
+            da = New SqlDataAdapter(comando)
+            dt = New DataTable()
+            da.Fill(ds)
+
+            dgvReporte.DataSource = ds.Tables(0).DefaultView
+
+        End If
+
+    End Sub
+
+    Private Sub rbtnReporte1_CheckedChanged(sender As Object, e As EventArgs) Handles rbtnReporte1.CheckedChanged
+        DateTimePicker1.Visible = False
+        DateTimePicker2.Visible = False
+        lblFechaFin.Visible = False
+        lblFechaIni.Visible = False
+    End Sub
+
+    Private Sub rbtnReporte2_CheckedChanged(sender As Object, e As EventArgs) Handles rbtnReporte2.CheckedChanged
+
+        DateTimePicker1.Visible = False
+        DateTimePicker2.Visible = False
+        lblFechaFin.Visible = False
+        lblFechaIni.Visible = False
+
+    End Sub
+
+    Private Sub rbtnReporte3_CheckedChanged(sender As Object, e As EventArgs) Handles rbtnReporte3.CheckedChanged
+        DateTimePicker1.Visible = False
+        DateTimePicker2.Visible = False
+    End Sub
+
+    Private Sub rbtnReporte4_CheckedChanged(sender As Object, e As EventArgs) Handles rbtnReporte4.CheckedChanged
+        DateTimePicker1.Visible = False
+        DateTimePicker2.Visible = False
+        lblFechaFin.Visible = False
+        lblFechaIni.Visible = False
+    End Sub
+
+    Private Sub rbtnReporte5_CheckedChanged(sender As Object, e As EventArgs) Handles rbtnReporte5.CheckedChanged
+        DateTimePicker1.Visible = False
+        DateTimePicker2.Visible = False
+        lblFechaFin.Visible = False
+        lblFechaIni.Visible = False
+    End Sub
+
+    Private Sub rbtnReporte6_CheckedChanged(sender As Object, e As EventArgs) Handles rbtnReporte6.CheckedChanged
+
+        DateTimePicker1.Visible = True
+        DateTimePicker2.Visible = True
+        lblFechaFin.Visible = True
+        lblFechaIni.Visible = True
 
     End Sub
 
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
 
-        comando = New SqlCommand("select * from b_detalle_delito", conn)
-        da = New SqlDataAdapter(comando)
-        dt = New DataTable()
-        da.Fill(ds)
+        ds.Tables.Clear()
 
-        dgvReporte.DataSource = ds.Tables(0).DefaultView
+        If rbtnReporte1.Checked = True Then
+            comando = New SqlCommand("select * from l_delincuente order by desc_apellido,desc_nombre", conn)
+            da = New SqlDataAdapter(comando)
+            dt = New DataTable()
+            da.Fill(ds)
 
+            '  dgvReporte.DataSource = ds.Tables(0).DefaultView
+
+        ElseIf rbtnReporte2.Checked = True Then
+
+            comando = New SqlCommand("select * from tb_rep_delito_cometido", conn)
+            da = New SqlDataAdapter(comando)
+            dt = New DataTable()
+            da.Fill(ds)
+
+            ' dgvReporte.DataSource = ds.Tables(0).DefaultView
+
+        ElseIf rbtnReporte3.Checked = True Then
+
+            comando = New SqlCommand("select * from tb_rep_delincuente_comuna", conn)
+            da = New SqlDataAdapter(comando)
+            dt = New DataTable()
+            da.Fill(ds)
+
+            ' dgvReporte.DataSource = ds.Tables(0).DefaultView
+
+        ElseIf rbtnReporte4.Checked = True Then
+
+            comando = New SqlCommand("select * from tb_rep_delincuente_ultima_vez", conn)
+            da = New SqlDataAdapter(comando)
+            dt = New DataTable()
+            da.Fill(ds)
+
+            ' dgvReporte.DataSource = ds.Tables(0).DefaultView
+
+        ElseIf rbtnReporte5.Checked = True Then
+
+            comando = New SqlCommand("select * from tb_rep_delincuente_parentesco order by desc_banda", conn)
+            da = New SqlDataAdapter(comando)
+            dt = New DataTable()
+            da.Fill(ds)
+
+            ' dgvReporte.DataSource = ds.Tables(0).DefaultView
+
+        ElseIf rbtnReporte6.Checked = True Then
+
+            Dim query
+            query = "select * from tb_rep_delito_ubicacion where fecha_delito between '" + fecha_ini + "' and '" + fecha_fin + "' order by desc_delito"
+            comando = New SqlCommand("select * from tb_rep_delito_ubicacion where fecha_delito between '" + fecha_ini + "' and '" + fecha_fin + "' order by desc_delito", conn)
+            da = New SqlDataAdapter(comando)
+            dt = New DataTable()
+            da.Fill(ds)
+
+            ' dgvReporte.DataSource = ds.Tables(0).DefaultView
+
+        End If
 
         Dim save As New SaveFileDialog
         save.Filter = "Archivo XML (*.xml) | *.xml"
@@ -56,4 +211,6 @@ Public Class ModuloReportes
         End If
 
     End Sub
+
+
 End Class
